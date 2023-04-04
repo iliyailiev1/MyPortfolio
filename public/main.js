7e12cbbc04167b7f0e67d387a2cd005eeaa0173f
Iliya Iliev <ilieviliya03@gmail.com>
2023-04-05 00:09:22 +0300

    https://github.com/iliyailiev1/MyPortfolio

---
+++ public/main.js
@@ -0,0 +1,76 @@
+var menu_visible = false;
+let menu = document.getElementById("nav");
+function mostrarOcultarMenu(){
+    if(menu_visible==false){
+        menu.style.display = "block";
+        menu_visible = true;
+    }
+    else{
+        menu.style.display = "none";
+        menu_visible = false;
+    }
+}
+let links = document.querySelectorAll("nav a");
+for(var x = 0; x <links.length;x++){
+    links[x].onclick = function(){
+        menu.style.display = "none";
+        menu_visible = false;
+    }
+}
+
+function crearBarra(id_barra){
+    for(i=0;i<=16;i++){
+        let div = document.createElement("div");
+        div.className = "e";
+        id_barra.appendChild(div);
+    }
+}
+
+let html = document.getElementById("html");
+crearBarra(html);
+let javascript = document.getElementById("javascript");
+crearBarra(javascript);
+let python = document.getElementById("python");
+crearBarra(python);
+let jquery = document.getElementById("jquery");
+crearBarra(jquery);
+
+
+let contadores = [-1,-1,-1,-1,-1,-1];
+let entro = false;
+
+function efectoHabilidades(){
+    var habilidades = document.getElementById("skills");
+    var distancia_skills = window.innerHeight - habilidades.getBoundingClientRect().top;
+    if(distancia_skills>=300 && entro==false){
+        entro = true;
+        const intervalHtml = setInterval(function(){
+            pintarBarra(html, 16, 0, intervalHtml);
+        },100);
+        const intervalJavascript = setInterval(function(){
+            pintarBarra(javascript, 10, 1, intervalJavascript);
+        },100);
+        const intervalPython = setInterval(function(){
+            pintarBarra(python, 10, 2, intervalPython);
+        },100);
+        const intervalJquery = setInterval(function(){
+            pintarBarra(jquery, 9, 3, intervalJquery);
+        },100);
+        
+    }
+}
+
+function pintarBarra(id_barra, cantidad, indice, interval){
+    contadores[indice]++;
+    x = contadores[indice];
+    if(x < cantidad){
+        let elementos = id_barra.getElementsByClassName("e");
+        elementos[x].style.backgroundColor = "#0099ff";
+    }else{
+        clearInterval(interval)
+    }
+}
+
+window.onscroll = function(){
+    efectoHabilidades();
+}
